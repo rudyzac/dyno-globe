54b25abeade54959234e432189b9f731e3eb95c9
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2022-08-12 02:08:55 +0200

    Use topojson world atlas file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const earthGeoJson = fetch('./custom.geo.json')
+const earthGeoJson = fetch('http://localhost:3000/world-atlas.json')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.log(error));
