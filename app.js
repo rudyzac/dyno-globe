31093baf3933b1d8f68092974c2bbe17b64630cb
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2022-08-12 07:49:22 +0200

    - Add topojson-client lib; - Get countries data

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const worldAtlas = fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+const countries = fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
   .then(
     response => { console.log("Got world atlas from CDN"); return response }, 
     reason => {
       console.log(`Cannot get world atlas from CDN because of: ${reason}`)
-      fetch('http://localhost:3000/world-atlas.json')
+      fetch('/world-atlas.json')
     })
   .then(data => data.json())
-  .then(atlasData => console.log(atlasData))
+  .then(worldAtlas => { console.log("Atlas data", worldAtlas); return worldAtlas }) // TODO: remove
+  .then(worldAtlas => {
+    const countries = topojson.feature(worldAtlas, worldAtlas.objects.countries);
+    console.log("Countries", countries);  //TODO: remove
+    return countries
+  })
   .catch(error => {
     console.log(error)
   })
