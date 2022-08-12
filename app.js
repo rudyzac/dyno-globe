c087f2c9b658908a296d9150c08b86daf105cbb7
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2022-08-12 07:25:21 +0200

    Get world atlas from CDN and fallback to our server

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const earthGeoJson = fetch('http://localhost:3000/world-atlas.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+const worldAtlas = fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+  .then(
+    response => { console.log("Got world atlas from CDN"); return response }, 
+    reason => {
+      console.log(`Cannot get world atlas from CDN because of: ${reason}`)
+      fetch('http://localhost:3000/world-atlas.json')
+    })
+  .then(data => data.json())
+  .then(atlasData => console.log(atlasData))
+  .catch(error => {
+    console.log(error)
+  })
