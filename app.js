14ed82b4aa925e2981d29eb1b3b56f7608a80c9d
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2022-08-12 08:55:58 +0200

    Scale and translate map

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const height = 600;
 const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 const g = svg.append('g');
 
-const projection = d3.geoMercator();
+const projection = d3.geoMercator().scale(100).translate([width/2, height/2]);
 const path = d3.geoPath(projection);
 
 fetch('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
